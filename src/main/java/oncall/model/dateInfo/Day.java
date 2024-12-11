84b050a2a7c6eb85d29b782cbfd4a17b57b22d24
Chaeyoung714 <202102760@hufs.ac.kr>
2024-12-11 17:55:21 +0900

    feat : 0. 평일, 공휴일 정보 저장 기능 구현

---
+++ src/main/java/oncall/model/dateInfo/Day.java
@@ -0,0 +1,19 @@
+package oncall.model.dateInfo;
+
+public enum Day {
+    SUNDAY(),
+    MONDAY(),
+    TUESDAY(),
+    WEDNESDAY(),
+    THURSDAY(),
+    FRIDAY(),
+    SATURDAY();
+
+
+//    public static boolean isWeekend(int date) {
+//        int remainderOfSevenOfDate = date % 7;
+//        // 토,일이 주말인 기준임
+//        return remainderOfSevenOfDate == SATURDAY.remainderOfSeven || remainderOfSevenOfDate == SUNDAY.remainderOfSeven;
+//    }
+
+}
---
+++ src/main/java/oncall/model/dateInfo/LegalHoliday.java
@@ -0,0 +1,29 @@
+package oncall.model.dateInfo;
+
+import static oncall.model.dateInfo.Month.AUGUST;
+import static oncall.model.dateInfo.Month.DECEMBER;
+import static oncall.model.dateInfo.Month.JANUARY;
+import static oncall.model.dateInfo.Month.JUNE;
+import static oncall.model.dateInfo.Month.MARCH;
+import static oncall.model.dateInfo.Month.MAY;
+import static oncall.model.dateInfo.Month.OCTOBER;
+
+public enum LegalHoliday {
+    신정(JANUARY, 1),
+    삼일절(MARCH, 1),
+    어린이날(MAY, 5),
+    현충일(JUNE, 6),
+    광복절(AUGUST, 15),
+    개천절(OCTOBER, 3),
+    한글날(OCTOBER, 9),
+    성탄절(DECEMBER, 25),
+    ;
+
+    private final Month month;
+    private final int date;
+
+    LegalHoliday(Month month, int date) {
+        this.month = month;
+        this.date = date;
+    }
+}
